30506ae6354f3dae8d1aa5907cda0eccde2e4336
sumight <1005546504@qq.com>
2015-09-16 22:33:50 +0800

    add

--- test.js
+++ test.js
@@ -2,4 +2,5 @@ var loadControllers = require('./app.js').loadControllers;
 
 loadControllers({host:'localhost',port:4000},function(controllers){
 // daf this is test new add
+// this is add add to test
 }) 
